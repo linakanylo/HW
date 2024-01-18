fc24ab94d46e3007aa436962cc4fe0791f59462b
Angelina Kanylo <142004037+linakanylo@users.noreply.github.com>
2024-01-18 20:22:06 +0200

    Update 3_1_hw.py
    Second commit

--- 3_1_hw.py
+++ 3_1_hw.py
@@ -1,17 +1,19 @@
-with open("C:\\Users\\Lenovo\\Desktop\\input.txt", 'r') as f:
+with open('input.txt', 'r') as f:
     for line in f:
 
         numbers = [int(num) for num in line.split()]
 
         for num in numbers:
             output = ""
-            if num % 2 == 0:
+            if num % 2 == 0 and num % 3 == 0:
+                output += "FizzBuzz"
+            elif num % 2 == 0:
                 output += "Fizz"
-            if num % 3 == 0:
+            elif num % 3 == 0:
                 output += "Buzz"
-
-            if not output:
+            else:
                 output = num
 
             print(output, end=' ')
         print()
+
